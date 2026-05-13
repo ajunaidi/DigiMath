21d79fe50f5faa4be41a0ddd88099023fb3a09fd
ajunaidi <100400624+ajunaidi@users.noreply.github.com>
2026-05-13 15:43:33 +0500

    Add files via upload

--- keys.js
+++ keys.js
@@ -2,6 +2,6 @@
 // Paste your keys here. This file is ignored by Git.
 
 window.DIGIMATH_KEYS = {
-    OPENCODE_ZEN_KEY: 'PASTE_YOUR_OPENCODE_ZEN_KEY_HERE',
+    OPENCODE_ZEN_KEY: 'Psk-DLS1zc89rzFlVlYZ7rsAeGEwh1HfxNAILbROIftRAIRPZAp8Muvb4Yz1SeJblkYcE',
     ANTHROPIC_API_KEY: 'PASTE_YOUR_ANTHROPIC_KEY_HERE'
 };
